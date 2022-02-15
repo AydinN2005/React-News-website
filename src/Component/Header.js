5c64e1ef8f512c1c71e9a02b0b518c391dacc686
aydin noroozi kasanag <aydinnoroozi.k@gmail.com>
2022-02-15 10:35:33 +0330

    header comp completed

--- src/Component/Header.js
+++ src/Component/Header.js
@@ -3,7 +3,8 @@ import './Component_Styles/Header.css'
 const Header = () => {
     return (
         <div className='headercontainer'>
-            <h1 className='headertitle h1--face'>React News</h1>
+            <h1 className='headertitle h1--face font--size--xlg'>React News</h1>
+            <h3 className='h3--face font--size--md headerdesc'>We are here for say latest news about <span className='font--size--xlg React-js'>React.js</span>😉</h3>
         </div>
     );
 };

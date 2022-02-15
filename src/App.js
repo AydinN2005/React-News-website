11a5857e656e0afa5615008c6c55cf429120ed87
aydin noroozi kasanag <aydinnoroozi.k@gmail.com>
2022-02-15 14:20:52 +0330

    left side bar completed

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
+import { render } from '@testing-library/react';
 import React from 'react';
+import { Routes , Route , BrowserRouter} from 'react-router-dom';
 import Header from './Component/Header';
+import LeftSideBar from './Component/LeftSideBar';
 import './style.css'
 const App = () => {
     return (
         <div>
-          <Header/>
+          <BrowserRouter>
+            <Routes>
+              <Route path='/' element={<div><Header/><LeftSideBar/></div>}/>
+          </Routes>
+          </BrowserRouter>
         </div>
     );
 };

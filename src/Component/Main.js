0b7890139be23b2a764d3cc6e8796b3546ade48b
aydin noroozi kasanag <aydinnoroozi.k@gmail.com>
2022-02-16 17:39:14 +0330

    leftsidebar's data fetching from json server and set article property to json file

--- src/Component/Main.js
+++ src/Component/Main.js
@@ -1,9 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { GetleftsidebarData } from '../_api/GetleftsidebarData';
 import './Component_Styles/Main.css'
 const Main = () => {
+    const [article , setArticle] = useState();
+    React.useEffect(()=>{
+        GetleftsidebarData((isOk , data)=>{
+            if(!isOk)
+            alert("We can't found this article ! may it deleted .")
+            else
+            setArticle(data) 
+        })
+    } , [])
     return (
         <div className='maincontainer'>
-            {/* <h1>Why we should use React for website's client view ? </h1> */}
+            
         </div>
     );
 };
